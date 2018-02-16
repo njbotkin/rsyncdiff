2defb1633958e00783af35775d97b1fb21025d03
Noah <njbotkin@gmail.com>
2018-02-15 22:13:49 -0600

    error catching, eslint-formatting

--- cli.js
+++ cli.js
@@ -1,43 +1,51 @@
 #! /usr/bin/env node
 
-const { diff } = require('./index')
-const cli = require('commander')
-const gitignore = require('parse-gitignore')
+const { diff } = require(`./index`)
+const cli = require(`commander`)
+const gitignore = require(`parse-gitignore`)
 
-function list(val) {
-  return val.split(',')
+async function main() {
+	function list(val) {
+		return val.split(`,`)
+	}
+
+	cli
+		.version(`0.1.0`)
+		.usage(`[options] <local> <remote>`)
+		.option(`--exclude <items>`, `(comma-delimited list) directories or files`, list, [])
+		.option(`--gitignore [path-to-git-ignore]`, `Excludes files from specified .gitignore file (defaults to working-directory .gitignore)`)
+		.option(`--include <items>`, `(comma-delimited list) remove items from exclude list`, list, [])
+		.on(`--help`, () => {
+			console.log(``)
+			console.log(`  Example:`)
+			console.log(``)
+			console.log(`    $ rsyncdiff localpath user@host:~/remotepath`)
+			console.log(``)
+		})
+		.parse(process.argv)
+
+	let exclude = cli.exclude
+	const include = cli.include
+
+	try {
+		if (cli.gitignore) {
+			const path = cli.gitignore === true ? `.gitignore` : cli.gitignore
+			exclude = exclude.concat(gitignore(path))
+		}
+
+		exclude = exclude.filter(e => include.indexOf(e) < 0)
+
+		if (cli.args.length < 2) {
+			throw `need local and remote paths`
+		}
+
+		await diff(cli.args[0], cli.args[1], {
+			exclude,
+		})
+	} catch (error) {
+		console.log(`rsyncdiff:`, error)
+		process.exit(1)
+	}
 }
 
-cli
-  .version('0.1.0')
-  .usage('[options] <local> <remote>')
-  .option('--exclude <items>', '(comma-delimited list) directories or files', list, [])
-  .option('--gitignore [path-to-git-ignore]', 'Excludes files from specified .gitignore file (defaults to working-directory .gitignore)')
-  .option('--include <items>', '(comma-delimited list) remove items from exclude list', list, [])
-  .on('--help', function(){
-	console.log('')
-	console.log('  Example:')
-	console.log('')
-	console.log('    $ rsyncdiff localpath user@host:~/remotepath')
-	console.log('')
-  })
-  .parse(process.argv)
-
-var exclude = cli.exclude
-var include = cli.include
-
-if(cli.gitignore) {
-	let path = cli.gitignore === true ? '.gitignore' : cli.gitignore
-	exclude = exclude.concat(gitignore(path))
-}
-
-exclude = exclude.filter(e => include.indexOf(e) < 0)
-
-if (cli.args.length < 2) {
-   console.error('need local and remote paths')
-   process.exit(1)
-}
-
-diff(cli.args[0], cli.args[1], {
-	exclude
-})
+main()
--- index.js
+++ index.js
@@ -1,139 +1,129 @@
-const chalk = require('chalk')
-const pify = require('pify')
-const { exec } = pify(require('child_process'))
+const chalk = require(`chalk`)
+const pify = require(`pify`)
+const { exec } = pify(require(`child_process`))
 
 const terminalWidth = (process.stdout && process.stdout.columns) ? process.stdout.columns : 80
 
 module.exports = {
-  diff: (local, remote, { exclude = [] }) => {
+	diff: (local, remote, { exclude = [] }) => {
+		exclude = exclude.reduce((a, e) => a + ` --exclude "` + e + `"`, ``)
 
-      exclude = exclude.reduce((a, e) => a+' --exclude "'+e+'"', '')
+		const flags = `-crlinz` // matches ALL different files
+		const reverse_flags = `-curlinz` // only matches files on remote that are newer
 
-      var flags = '-crlinz' // matches ALL different files
-      var reverse_flags = '-curlinz' // only matches files on remote that are newer
+		return Promise.all([
+			exec(`rsync ` + local + ` ` + remote + ` ` + exclude + ` ` + flags),
+			exec(`rsync ` + remote + ` ` + local + ` ` + exclude + ` ` + reverse_flags),
+		])
+			.then(res => {
+				console.log(`\n key:  ` + chalk.bgCyan(`  `) + chalk.bold.cyan(` new  `) + chalk.bgYellow(`  `) + chalk.bold.yellow(` newer `) + `\n`)
 
-      return Promise.all([
-        exec('rsync '+local+' '+remote+' '+exclude+' '+flags),
-        exec('rsync '+remote+' '+local+' '+exclude+' '+reverse_flags)
-      ])
-      .then(function(res) {
+				function jsonify(string) {
+					return string.split(`\n`).map(c => {
+						const s = c.indexOf(` `)
+						return { gibber: c.substring(0, s), name: c.substring(s + 1) }
+					})
+				}
 
-        console.log('\n key:  ' + chalk.bgCyan('  ') + chalk.bold.cyan(' new  ') + chalk.bgYellow('  ') + chalk.bold.yellow(' newer ') + '\n')
+				const here = jsonify(res[0] || ``)
+				const there = jsonify(res[1] || ``)
 
-        function jsonify (string) {
-          return string.split('\n').map(function(c) {
-            var s = c.indexOf(' ')
-            return {gibber: c.substring(0, s), name: c.substring(s+1)}
-          })
-        }
+				here.pop()
+				there.pop()
 
-        var here = jsonify(res[0] || '')
-        var there = jsonify(res[1] || '')
+				const both = []
 
-        here.pop()
-        there.pop()
+				// merge here and there
+				here.forEach(e => {
+					both[e.name] = { here: e.gibber, there: `` }
+				})
+				there.forEach(e => {
+					if (both[e.name]) {
+						both[e.name].there = e.gibber
+					} else {
+						both[e.name] = { here: ``, there: e.gibber }
+					}
+				})
 
-        var both = []
+				const table = []
+				// convert obj to array
+				for (const i in both) {
+					table.push({ name: i, row: both[i] })
+				}
 
-        // merge here and there
-        here.forEach(function (e) {
-          both[e.name] = {here: e.gibber, there: ''}
-        })
-        there.forEach(function (e) {
-          if(both[e.name]) {
-            both[e.name].there = e.gibber
-          } else {
-            both[e.name] = {here: '', there: e.gibber}
-          }
-        })
+				if (table.length === 0) {
+					return Promise.reject(`No differences.`)
+				}
 
-        var table = []
-        // convert obj to array
-        for(var i in both) {
-          table.push({name: i, row: both[i]})
-        }
+				// print side-by-side
+				const colwidth = Math.floor((terminalWidth - 6) / 2)
+				const colcenter = colwidth / 2
 
-        if(table.length === 0) {
-            return Promise.reject('No differences.')
-        }
+				function fit(str) {
+					let strProcessed
+					if (str.length > colwidth) {
+						// truncate middle
+						const overflow = str.length - colwidth + 1
+						const remove = str.substr(colcenter, overflow)
+						strProcessed = str.replace(remove, `…`)
+					} else {
+						// right-pad
+						strProcessed = str
+						let l = colwidth - str.length
+						for (l; l > 0; l--) {
+							strProcessed += ` `
+						}
+					}
+					return strProcessed
+				}
 
-        // print side-by-side
-        var colwidth = Math.floor((terminalWidth - 6) / 2)
-        var colcenter = colwidth/2
+				console.log(` ` + fit(local) + `    ` + fit(remote) + `\n`)
 
-        function process(str) {
-          var strProcessed
-          if(str.length > colwidth) {
-            // truncate middle
-            var overflow = str.length - colwidth + 1
-            var remove = str.substr( colcenter, overflow )
-            strProcessed = str.replace(remove, '…')
-          } else {
-            // right-pad
-            strProcessed = str
-            var l = colwidth - str.length
-            for(l; l > 0; l--) strProcessed += ' '
-          }
-          return strProcessed
-        }
+				// draw table
+				table.forEach(e => {
+					const r = e.row
 
-        console.log(' ' + process(local) + '    ' + process(remote) + '\n')
+					if (r.here) {
+						if (r.here[1] === `f`) {
+							// file
+							if (r.here[2] === `c`) {
+								// change event
+								if (r.there) {
+									// file newer on remote
+									console.log(` ` + chalk.bold.black(fit(e.name)) + `    ` + chalk.bold.yellow(fit(e.name)))
+								} else {
+									// file newer on local
+									console.log(` ` + chalk.bold.yellow(fit(e.name)) + `    ` + chalk.bold.black(fit(e.name)))
+								}
+							}
 
-        // draw table
-        table.forEach(function(e) {
-          let r = e.row
+							if (r.here[2] === `+`) {
+								// file does not exist on remote
+								console.log(` ` + chalk.bold.cyan(fit(e.name)) + `    ` + fit(``))
+							}
+						}
 
-          if(r.here) {
+						if (r.here[1] === `d`) {
+							// directory does not exist on remote
+							console.log(` ` + chalk.bold.cyan(fit(e.name)) + `    ` + fit(``))
+						}
+					} else if (r.there) {
+						if (r.there[1] === `f` && r.there[2] === `+`) {
+							// file does not exist on local
+							console.log(` ` + fit(``) + `    ` + chalk.bold.cyan(fit(e.name)))
+						}
+						if (r.there[1] === `d`) {
+							// directory does not exist on local
+							console.log(` ` + fit(``) + `    ` + chalk.bold.cyan(fit(e.name)))
+						}
+					}
+				})
 
-            if(r.here[1] === 'f') {
-              // file
-              if(r.here[2] === 'c') {
-                // change event
-                if(r.there) {
-                  // file newer on remote
-                  console.log(' ' + chalk.bold.black(process(e.name)) + '    ' + chalk.bold.yellow(process(e.name)))
-                } else {
-                  // file newer on local
-                  console.log(' ' + chalk.bold.yellow(process(e.name)) + '    ' + chalk.bold.black(process(e.name)))
-                }
-              }
+				if (here.length == 0) {
+					throw `Nothing to transfer!`
+				}
 
-              if(r.here[2] === '+') {
-                // file does not exist on remote
-                console.log(' ' + chalk.bold.cyan(process(e.name)) + '    ' + process(''))
-              }
-            }
-
-            if(r.here[1] === 'd') {
-              // directory does not exist on remote
-              console.log(' ' + chalk.bold.cyan(process(e.name)) + '    ' + process(''))
-            }
-
-          }
-          else if(r.there) {
-
-            if(r.there[1] === 'f' && r.there[2] === '+') {
-              // file does not exist on local
-              console.log(' ' + process('') + '    ' + chalk.bold.cyan(process(e.name)))
-            }
-            if(r.there[1] === 'd') {
-              // directory does not exist on local
-              console.log(' ' + process('') + '    ' + chalk.bold.cyan(process(e.name)))
-            }
-
-          }
-        })
-
-        if(here.length == 0) {
-          return Promise.reject('Nothing to transfer!')
-        }
-
-        return true
-
-      })
-      .catch(function(e) {
-        console.log(e)
-        return {err: e}
-      })
-  }
+				return 0
+			})
+	},
 }
